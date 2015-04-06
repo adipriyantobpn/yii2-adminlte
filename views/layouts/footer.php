4835b37ea2edd8892e6d6db74b8e3e3484e2ca75
Adi Priyanto <adipriyanto.bpn@gmail.com>
2015-04-07 01:25:13 +0700

    prepare main layout : prepare footer

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,8 +1,25 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+/* @var $adminlteDirAsset string */
+
+$companyName = isset(Yii::$app->params['companyName'])
+    ? Yii::$app->params['companyName']
+    : '';
+$companySite = isset(Yii::$app->params['companySite'])
+    ? Yii::$app->params['companySite']
+    : '#';
+$footerMessage = isset($this->params['footerMessage'])
+    ? $this->params['footerMessage']
+    : '';
+?>
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        Anything you want
+        <?= $footerMessage ?>
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2015 <a href="#">Company</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; <?= date('Y') ?> <?= Html::a($companyName, $companySite) ?>.</strong> All rights reserved.
 </footer>
